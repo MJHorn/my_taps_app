6d212e5fec887899906761ff1e84b8545a5dcd4e
MJHorn <ziggaroth@gmail.com>
2016-04-02 17:26:16 +1100

    Modified templates to display posts from database.

--- taplists/views.py
+++ taplists/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Tap
 
 def tap_list(request):
-    return render(request, 'taplists/tap_list.html', {})
+    taps = Tap.objects.order_by('-rating')[:5]
+    return render(request, 'taplists/tap_list.html', {'taps': taps})
