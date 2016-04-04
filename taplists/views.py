d0c565b265ebf06ead6934c6bb7d1669c804d688
MJHorn <ziggaroth@gmail.com>
2016-04-04 14:20:11 +1000

    automated db update

--- taplists/views.py
+++ taplists/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Tap
 
 def tap_list(request):
-    taps = Tap.objects.order_by('-rating')[:5]
+    taps = Tap.objects.order_by('-rating')[:10]
     return render(request, 'taplists/tap_list.html', {'taps': taps})
