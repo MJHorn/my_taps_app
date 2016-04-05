6fab29984568d4632e3b168148f1af40def108e4
MJHorn <ziggaroth@gmail.com>
2016-04-05 11:13:18 +1000

    automated db update

--- taplists/filterset.py
+++ taplists/filterset.py
@@ -1,14 +1,37 @@
 import django_filters
 from .models import Tap
+from django import forms
 
 BARLIST=(
-    ('tworow', 'Two Row'),
-    ('thealehouseproject', 'Alehouse'),
+    ('beerdeluxe', 'Beer Deluxe'),
+    ('beerdeluxehawthorn', 'Beer Deluxe Hawthorn'),
+    ('beermash', 'Beermash'),
+    ('belgianbeercafemelbourne', 'Belgian Beer Cafe'),
+    ('boatrockerbarrelroom', 'Boatrocker Barrel Room'),
+    ('boilermakerhouse', 'Boilermaker House'),
+    ('brotherburgerandthemarvellousbrew', 'Brother Burger'),
+    ('brotherburgerandthemarvellousbrewsouthyarra', 'Brother Burger South Yarra'),
+    ('carwyncellars', 'Carwyn Cellars'),
+    ('eastofeverything', 'East of Everything'),
+    ('forestershall', "Forester's Hall"),
+    ('foxinthecorn', 'Fox in the Corn'),
+    ('grapeandgrain', 'Grape and Grain'),
+    ('littlehop', 'Little Hop'),
+    ('moondogbrewery', 'Moon Dog Brewery'),
+    ('sunmothcanteenbar', 'Sun Moth'),
+    ('thealehouseproject', 'The Alehouse Project'),
+    ('thecatfish', 'The Catfish'),
+    ('thegertrudehotel', 'The Gertrude Hotel'),
+    ('thelocaltaphousestk', 'The Local Taphouse'),
+    ('theroyston', 'The Royston'),
+    ('trubru', 'TRUBRU'),
+    ('twobirdsbrewing', 'Two Birds Brewing'),
+    ('upinsmoke', 'Up In Smoke'),
 )
 
 
 class TapFilter(django_filters.FilterSet):
-    bar = django_filters.MultipleChoiceFilter(choices=BARLIST)
+    bar = django_filters.MultipleChoiceFilter(label="", widget=forms.CheckboxSelectMultiple, choices=BARLIST,)
 
     class Meta:
         model = Tap
